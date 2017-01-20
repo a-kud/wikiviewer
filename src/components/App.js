61ee619ce363d42602de6900e18b91f34ecdc73d
Andrew Kud <kud@mm.st>
2017-01-20 23:36:35 +0200

    center #search-form

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import SearchForm from "./SearchForm";
 import SearchResults from "./SearchResults";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchResults: {},
+        };
+    }
     render() {
         return(
-            <div>
+            <div className="app">
             <SearchForm />
             <SearchResults />
             </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,9 +4,11 @@ class SearchForm extends React.Component {
     render() {
         return(
             <form id="search-form">
-                <input type="text"></input>
-                <button type="button" value="update">Search</button>
-                <button type="button" value="delete">Random Article</button>
+                <input type="text" id="seachbar"></input>
+                <button type="button" id="btn-search" onClick={() => {
+                    alert("Search");
+                }}>Search</button>
+                <button type="button" id="btn-random">Random Article</button>
             </form>
         );
     }
