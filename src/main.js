f5ec50ce282bc00477c4f0f8783ee5a514f56add
Andrew Kud <kud@mm.st>
2017-01-20 21:42:25 +0200

    rename folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
-require("./styles/main.sass");
+require("./sass/main.sass");
 
 const container = document.querySelector(".container");
 
