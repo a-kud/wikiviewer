48cbb401a475a1c2d8ce9cf1f5361fceb7bb1635
Andrew Kud <kud@mm.st>
2017-01-22 03:12:47 +0200

    add method to retrieve search results

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,23 @@ class App extends React.Component {
         super();
         this.state = {
             searchResults: {},
+            searchTerm: null,
         };
     }
+
+    //term - string
+    // returns Object
+    getSearchResults(term) {
+        let baseUrl = "https://en.wikipedia.org/w/api.php",
+            urlSearch = `?action=query&list=search&
+                          srsearch=${term}&srlimit=50&format=json`;
+        let request = new Request(baseUrl + urlSearch, {mode: "cors"}),
+            result;
+
+        result = fetch(request).then((resp) => resp.json()).then((json) => json);
+        return result;
+    }
+    
     render() {
         return(
             <div className="app">
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,19 +2,6 @@ import React from "react";
 
 class SearchForm extends React.Component {
 
-    //term - string
-    // returns Object
-    getSearchResults(term) {
-        let baseUrl = "https://en.wikipedia.org/w/api.php",
-            urlSearch = `?action=query&list=search&
-                          srsearch=${term}&srlimit=50&format=json`;
-        let request = new Request(baseUrl + urlSearch, {mode: "cors"}),
-            result;
-
-        result = fetch(request).then((resp) => resp.json()).then((json) => json);
-        return result;
-    }
-
     render() {
         return(
             <form id="search-form">
