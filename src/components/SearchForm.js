a6a1b890e5768b49b941eb4e88ad1b3b6e53c9bf
Andrew Kud <kud@mm.st>
2017-01-23 23:38:09 +0200

    add handleClick method

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,13 +2,15 @@ import React from "react";
 
 class SearchForm extends React.Component {
 
+    handleClick() {
+        this.props.getSearch(this.props.searchTerm);
+    }
+
     render() {
         return(
             <form id="search-form">
                 <input type="text" id="seachbar" onChange={(e) => this.props.onChange(e)}></input>
-                <button type="button" id="btn-search" onClick={() => {
-                    this.props.getSearch(this.props.searchTerm);
-                }}>Search</button>
+                <button type="button" id="btn-search" onClick={() => this.handleClick() }>Search</button>
 
                 <a href="https://en.wikipedia.org/wiki/Special:Random" target="_blank">
                     <button type="button" id="btn-random">Random Article</button>
