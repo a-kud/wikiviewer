a4e0f7b229110b75a7a908cda84a9229d88734d3
Andrew Kud <kud@mm.st>
2017-01-25 10:01:16 +0200

    add props snippet and hyperlink to article

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,19 +3,17 @@ import SearchRow from "./SearchRow"
 
 class SearchResults extends React.Component {
 
-    parseSnippet(s) {
-        //let re = /<\/?span\sclass="\w+">*(\w+)<\/span\>/
-        let parser = new DOMParser();
-        let htmlDoc = parser.parseFromString(s, "text/html");
-        // console.log(htmlDoc.getElementsByTagName("body"));
-        return htmlDoc.getElementsByTagName("body")[0].innerHTML;
-    }
-
     render() {
-        let snippet = this.props.searchData ?  this.props.searchData.query.search[0].snippet : null;
+        // console.log(json)
         let numOfRows = this.props.searchData ?  this.props.searchData.query.search.length : null;
         if (numOfRows) {
-            let rows = [...Array(numOfRows)].map((x, i) => <SearchRow  key={i + 1}  />)
+            let json = this.props.searchData;
+            let search = this.props.searchData.query.search;
+            let rows = [...Array(numOfRows)].map((x, i) => {
+                return <SearchRow
+                    key={i + 1}
+                    link={`https://en.wikipedia.org/wiki/${search[i].title}`}
+                    snippet={search[i].snippet} />})
             return(
                 <div className="search-results"/* dangerouslySetInnerHTML={{__html: snippet}}*/>
                     {rows}
